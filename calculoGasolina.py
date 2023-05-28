3cfbc44b1dd2bef06b9fd0cf47a7641bbbe2d4b8
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-05-28 15:29:59 -0300

    criado calculo de comparacao gas e al

---
+++ calculoGasolina.py
@@ -0,0 +1,2 @@
+distancia = float(input("Informe a distância a ser percorrida em quilómetros com ponto e não virgula: "))
+consumo_medio = float(input("Informe o consumo médio de combustivel do veiculo: "))
