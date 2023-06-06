a5b4dcfce2baf13af28162f7dd77e589ecd46ae4
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-06-06 10:21:05 -0300

    otimizado calculadoras

--- alcoolOuGasolina.py
+++ alcoolOuGasolina.py
@@ -1,21 +1,22 @@
-#Segundo o engenheiro Gabriel Branco, especialista em motores, um veículo abastecido com etanol rende, em média, 30% a menos do que se estivesse com gasolina
 #Calculo de álcool e gasolina
 alcool = float(input("Digite o valor do álcool por litro(Etanol) em reais, com ponto, não virgula: R$:"))
 gasolina = float(input("Digite o valor da gasolina por litro em reais, com ponto, não virgula: R$:"))
 
 def verificacao (al,gas):
-  rendimento = al/gas
+  #verificando os valores inseridos pelo usuario
+  if al <= 0 and gas <= 0:
+    resultado = "Digite valores maiores que zero"
+  else:
+    rendimento = al/gas
   #comparando qual é melhor
   if rendimento > 0 and rendimento <= 0.7:
     resultado ="Vale mais a pena abastecer com álcool!"
-  if rendimento > 0.7:
+  else:
     resultado = "Vale mais a pena abastecer com gasolina!"
   
   return resultado
 
-#verificando valores digitados pelo usuário 
-if gasolina > 0 and alcool > 0:
-  apresentar = verificacao(alcool,gasolina)
-  print(apresentar)
-else:
-  print("Digite valores maiores que 0")
+calculo = verificacao (alcool, gasolina)
+print(calculo)
+
+#Segundo o engenheiro Gabriel Branco, especialista em motores, um veículo abastecido com etanol rende, em média, 30% a menos do que se estivesse com gasolina
